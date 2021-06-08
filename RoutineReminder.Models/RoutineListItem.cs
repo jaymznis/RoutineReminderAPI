50034a40ce6f1e29f8c0ae8aca9e839dfb33c5df
Jakob Amstutz <amstutzjakob@gmail.com>
2021-06-08 13:53:21 -0400

    Added routineservice

---
+++ RoutineReminder.Models/RoutineListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoutineReminder.Models
+{
+    public class RoutineListItem
+    {
+        public int RoutineId { get; set; }
+        public string RoutineName { get; set; }
+       
+    }
+}
