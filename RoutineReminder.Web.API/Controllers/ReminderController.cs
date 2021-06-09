6426c07cc74f5680d1aa25f8148c8641d337ffdb
Jakob Amstutz <amstutzjakob@gmail.com>
2021-06-09 15:13:40 -0400

    Created controller for reminder

--- RoutineReminder.Web.API/Controllers/ReminderController.cs
+++ RoutineReminder.Web.API/Controllers/ReminderController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RoutineReminder.Models;
+using RoutineReminder.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -12,27 +14,27 @@ namespace RoutineReminder.Web.API.Controllers
     {
         private ReminderService CreateReminderService()
         {
-            var noteService = new RoutineItemService();
-            return noteService;
+            var remindService = new ReminderService();
+            return remindService;
         }
 
         [HttpGet]
         public IHttpActionResult GetAll()
         {
-            RoutineItemService routineItemService = CreateRoutineItemService();
-            var routineItems = routineItemService.GetRoutineItems();
-            return Ok(routineItems);
+            ReminderService reminderService = CreateReminderService();
+            var reminders = reminderService.GetAllReminders();
+            return Ok(reminders);
         }
 
         [HttpPost]
-        public IHttpActionResult Post(RoutineItemCreate routineItem)
+        public IHttpActionResult Post(ReminderCreate reminder)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            var service = CreateRoutineItemService();
+            var service = CreateReminderService();
 
-            if (!service.CreateRoutineItem(routineItem))
+            if (!service.CreateReminder(reminder))
                 return InternalServerError();
 
             return Ok();
@@ -41,31 +43,31 @@ namespace RoutineReminder.Web.API.Controllers
         [HttpGet]
         public IHttpActionResult GetByID(int id)
         {
-            RoutineItemService routineItemService = CreateRoutineItemService();
-            var routineItem = routineItemService.GetRoutineItemById(id);
-            return Ok(routineItem);
+            ReminderService reminderService = CreateReminderService();
+            var reminder = reminderService.GetReminderById(id);
+            return Ok(reminder);
         }
 
         [HttpPut]
-        public IHttpActionResult Put(RoutineItemEdit routineItem)
+        public IHttpActionResult Put(ReminderEdit reminder)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            var service = CreateRoutineItemService();
+            var service = CreateReminderService();
 
-            if (!service.UpdateRoutineItem(routineItem))
+            if (!service.UpdateReminder(reminder))
                 return InternalServerError();
 
             return Ok();
         }
 
         [HttpDelete]
-        public IHttpActionResult DeleteByID(int id)
+        public IHttpActionResult DeleteById(int id)
         {
-            var service = CreateRoutineItemService();
+            var service = CreateReminderService();
 
-            if (!service.DeleteRoutineItem(id))
+            if (!service.DeleteReminder(id))
                 return InternalServerError();
 
             return Ok();
