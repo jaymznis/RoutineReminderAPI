99434ec3e0753320d445178c4916debb228b26b6
Jakob Amstutz <amstutzjakob@gmail.com>
2021-06-09 10:18:05 -0400

    created ReminderDetail

---
+++ RoutineReminder.Models/ReminderDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoutineReminder.Models
+{
+    class ReminderDetail
+    {
+        public int ReminderId { get; set; }
+        public string ReminderName { get; set; }
+        public string ReminderDesc { get; set; }
+        public DateTime ReminderTime { get; set; }
+        public virtual List<RoutineItemListItem> RoutineItems { get; set; }
+    }
+}
