c9ddcfb9e499d650967ae8455754cc5a268f16f1
Jakob Amstutz <amstutzjakob@gmail.com>
2021-06-09 13:39:16 -0400

    Reminder data class

---
+++ RoutineReminder.Data/Reminder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoutineReminder.Data
+{
+    public class Reminder
+    {
+        [Key]
+        public int ReminderId { get; set; }
+
+        [ForeignKey(nameof(Routine))]
+        public int? RoutineId { get; set; }
+        public virtual Routine Routine { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public string ReminderName { get; set; }
+
+        public string ReminderDesc { get; set; }
+
+        public DateTime ReminderTime { get; set; }
+
+        public virtual List<Reminder> Reminders { get; set; }
+
+    }
+}
---
+++ RoutineReminder.Web.API/Controllers/ReminderController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RoutineReminder.Web.API.Controllers
+{
+    [Authorize]
+    public class ReminderController : ApiController
+    {
+        private ReminderService CreateReminderService()
+        {
+            var noteService = new RoutineItemService();
+            return noteService;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            RoutineItemService routineItemService = CreateRoutineItemService();
+            var routineItems = routineItemService.GetRoutineItems();
+            return Ok(routineItems);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(RoutineItemCreate routineItem)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateRoutineItemService();
+
+            if (!service.CreateRoutineItem(routineItem))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetByID(int id)
+        {
+            RoutineItemService routineItemService = CreateRoutineItemService();
+            var routineItem = routineItemService.GetRoutineItemById(id);
+            return Ok(routineItem);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Put(RoutineItemEdit routineItem)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateRoutineItemService();
+
+            if (!service.UpdateRoutineItem(routineItem))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteByID(int id)
+        {
+            var service = CreateRoutineItemService();
+
+            if (!service.DeleteRoutineItem(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
