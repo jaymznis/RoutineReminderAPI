cde3082f992f1f3a73aa2a05dbd63455cc2229f9
Taylor <taylorfulner@gmail.com>
2021-06-14 14:53:44 -0400

    added shopping list and updated key designation

--- RoutineReminder.Data/Routine_RoutineItem.cs
+++ RoutineReminder.Data/Routine_RoutineItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace RoutineReminder.Data
 {
    public class Routine_RoutineItem
     {
+        [Key]
         public int R_RI_Id { get; set; }
 
         public int RoutineId { get; set; }
---
+++ RoutineReminder.Data/ShoppingList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoutineReminder.Data
+{
+    public class ShoppingList
+    {
+        [Key]
+        public int ShoppingListId { get; set; }
+
+        [Required]
+        public string ShoppingListName { get; set; }
+
+        public string ShoppingListDesc { get; set; }
+
+        public List<string> ShoppingListItem { get; set; }
+    }
+}
