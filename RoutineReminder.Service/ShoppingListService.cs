669335c98faf226e0a3c3b0d4a4575bf9357f87b
Taylor <taylorfulner@gmail.com>
2021-06-14 18:02:41 -0400

    added shopping list service and controller

---
+++ RoutineReminder.Service/ShoppingListService.cs
@@ -0,0 +1,54 @@
+﻿using RoutineReminder.Data;
+using RoutineReminder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoutineReminder.Service
+{
+    public class ShoppingListService
+    {
+        private readonly Guid _userId;
+        public ShoppingListService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreatePost(ShoppingListCreate model)
+        {
+            var entity = new ShoppingList()
+            {
+                ShoppingListName = model.Title,
+                ShoppingListItem = model.Items
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.ShoppingLists.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<ShoppingListItem> GetShoppingLists()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx
+                    .ShoppingLists
+                    .Select
+                    (
+                        e => new ShoppingListItem
+                        {
+                            ShoppingListId = e.ShoppingListId,
+                            ShoppingListName = e.ShoppingListName
+                        }
+                    );
+                return query.ToArray();
+            }
+        }
+
+
+    }
+}
---
+++ RoutineReminder.Web.API/Controllers/ShoppingListController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity;
+using RoutineReminder.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RoutineReminder.Web.API.Controllers
+{
+    [Authorize]
+    public class ShoppingListController : ApiController
+    {
+        private ShoppingListService CreateShoppingListService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var shoppingListService = new ShoppingListService(userId);
+            return shoppingListService;
+        }
+    }
+}
